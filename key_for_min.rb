84917e9540dd5b778dce2f994893a2ddcc239431
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-21 15:31:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_value = 1000
   name_hash.each do |key, value|
-    
+    if value < min_value
+      min_value = value
+      min_value_key = key
+    end
   end
+  min_value_key
 end
